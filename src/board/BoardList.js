084f0ade3a139ce42f1aa244b8a2ae2cd9a22cf9
rlatkdgns <tkdgns96@naver.com>
2023-11-17 16:01:13 +0900

    modified:   src/board/BoardDetail.js 	modified:   src/board/BoardList.js

--- src/board/BoardList.js
+++ src/board/BoardList.js
@@ -21,6 +21,7 @@ const BoardList = ({ history }) => {
                 history.push('/login');
             }                
         });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
